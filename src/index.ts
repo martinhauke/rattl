6d84961683744782eb84d4dfcde88ee3ae0d5a21
Martin Hauke <martin.hauke@sandstorm.de>
2022-10-19 08:53:10 +0200

    Add option to show external resources

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,21 @@ import yargs from 'yargs/yargs'
 type Arguments = {
   url: string
   errorsOnly?: boolean
+  showExternalResources?: boolean
 }
 
-const argv: Arguments = yargs(process.argv.slice(2)).options({
-  url: {type: 'string', demandOption: true, alias: 'u', nargs: 1},
-  errorsOnly: {type: 'boolean'},
-}).parseSync()
+const argv: Arguments = yargs(process.argv.slice(2))
+  .options({
+    url: { type: 'string', demandOption: true, alias: 'u', nargs: 1 },
+    errorsOnly: { type: 'boolean' },
+    showExternalResources: { type: 'boolean' },
+  })
+  .parseSync()
 
-const {url, errorsOnly} = argv
+const { url, errorsOnly, showExternalResources } = argv
 if (url === undefined) {
   console.log('Please provide a url')
   exit(1)
 }
 
-crawl(url, errorsOnly === true)
+crawl(url, errorsOnly === true, showExternalResources === true)
